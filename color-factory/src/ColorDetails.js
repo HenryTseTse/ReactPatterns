f22df7c9f5de23fcec5d8050b19fb940555ed204
Henry Tse <ht688@nyu.edu>
2024-02-21 19:58:50 -0500

    final commit

---
+++ color-factory/src/ColorDetails.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, Navigate, useParams } from "react-router-dom";
+
+function ColorDetails({colors}) {
+    const { color } = useParams();
+    if (!colors) return <Navigate to="/colors" />
+    
+    
+    if (color) {
+        const currColor = colors.find(c => c.name.toLowerCase() === color.toLowerCase());
+
+        return (
+            <div style={{backgroundColor: currColor.name}}>
+                <h1>This is {currColor.name}</h1>
+                <h1>Isn't it beautiful?</h1>
+                <Link to="/colors">Go Back</Link>
+            </div>
+        )
+    }
+
+    return null;
+}
+
+export default ColorDetails;
---
+++ color-factory/src/NewColorForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { v4 as uuid } from 'uuid';
+import { useNavigate, Link } from "react-router-dom";
+
+function NewColorForm({createColor}) {
+    const [formData, setFormData] = useState({
+        name: ""
+    })
+
+    const navigate = useNavigate();
+
+    function isValidColor(colorString) {
+        let s = new Option().style;
+        s.color = colorString;
+
+        return s.color === colorString?.toLowerCase();
+    }
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData(formData => ({...formData, [name]: value}))
+    }
+
+    const input = (e) => {
+        e.preventDefault();
+        if(isValidColor(formData.name)) {
+            createColor({...formData, id: uuid()});
+            setFormData({name: ""})
+            navigate("/");
+        } else {
+            alert("Input was not a valid color.")
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={input}>
+                <div>
+                    <label htmlFor="name">Color name:</label>
+                    <input 
+                        onChange={handleChange}
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        id="name"
+                    />
+                </div>
+                <button id="newBoxButton">Add new color</button>
+            </form>
+            <Link to="/colors">Nevermind, lets go back.</Link>
+        </div>
+    )
+}
+
+export default NewColorForm;
